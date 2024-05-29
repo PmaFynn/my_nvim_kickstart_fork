41091cd68b05d188ba8a09ec037bc7f4b29ffafc
Fynn Friedrichs <fynn091209@proton.me>
2024-05-29 16:25:09 +0200

    basic setup

---
+++ lua/custom/plugins/dashboard.lua
@@ -0,0 +1,81 @@
+return {
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup {
+        theme = 'doom',
+        config = {
+          header = {
+            [[                                                                                ]],
+            [[ =================     ===============     ===============   ========  ======== ]],
+            [[ \\ . . . . . . .\\   //. . . . . . .\\   //. . . . . . .\\  \\. . .\\// . . // ]],
+            [[ ||. . ._____. . .|| ||. . ._____. . .|| ||. . ._____. . .|| || . . .\/ . . .|| ]],
+            [[ || . .||   ||. . || || . .||   ||. . || || . .||   ||. . || ||. . . . . . . || ]],
+            [[ ||. . ||   || . .|| ||. . ||   || . .|| ||. . ||   || . .|| || . | . . . . .|| ]],
+            [[ || . .||   ||. _-|| ||-_ .||   ||. . || || . .||   ||. _-|| ||-_.|\ . . . . || ]],
+            [[ ||. . ||   ||-'  || ||  `-||   || . .|| ||. . ||   ||-'  || ||  `|\_ . .|. .|| ]],
+            [[ || . _||   ||    || ||    ||   ||_ . || || . _||   ||    || ||   |\ `-_/| . || ]],
+            [[ ||_-' ||  .|/    || ||    \|.  || `-_|| ||_-' ||  .|/    || ||   | \  / |-_.|| ]],
+            [[ ||    ||_-'      || ||      `-_||    || ||    ||_-'      || ||   | \  / |  `|| ]],
+            [[ ||    `'         || ||         `'    || ||    `'         || ||   | \  / |   || ]],
+            [[ ||            .===' `===.         .==='.`===.         .===' /==. |  \/  |   || ]],
+            [[ ||         .=='   \_|-_ `===. .==='   _|_   `===. .===' _-|/   `==  \/  |   || ]],
+            [[ ||      .=='    _-'    `-_  `='    _-'   `-_    `='  _-'   `-_  /|  \/  |   || ]],
+            [[ ||   .=='    _-'          `-__\._-'         `-_./__-'         `' |. /|  |   || ]],
+            [[ ||.=='    _-'                                                     `' |  /==.|| ]],
+            [[ =='    _-'                                                            \/   `== ]],
+            [[ \   _-'                                                                `-_   / ]],
+            [[  `''                                                                      ``'  ]],
+            [[                                                                                ]],
+          },
+          center = {
+            {
+              icon_hl = 'red',
+              desc = 'Find Files           ',
+              desc_hl = 'string',
+              key = '<leader> f f',
+              key_hl = 'number',
+              key_format = ' %s', -- remove default surrounding `[]`
+              action = 'lua print(2)',
+            },
+            {
+              desc = 'Find String',
+              desc_hl = 'string',
+              key = '<leader> /',
+              key_hl = 'number',
+              key_format = ' %s', -- remove default surrounding `[]`
+              action = 'lua print(3)',
+            },
+            {
+              desc = 'Telescope Builtin',
+              desc_hl = 'string',
+              key = '<leader> s s',
+              key_hl = 'number',
+              key_format = ' %s', -- remove default surrounding `[]`
+              action = 'lua print(3)',
+            },
+            {
+              desc = 'Find recently opened files',
+              desc_hl = 'string',
+              key = '<leader> ?',
+              key_hl = 'number',
+              key_format = ' %s', -- remove default surrounding `[]`
+              action = 'lua print(3)',
+            },
+            {
+              desc = 'Current buffer fuzzy search',
+              desc_hl = 'string',
+              key = '<leader> s /',
+              key_hl = 'number',
+              key_format = ' %s', -- remove default surrounding `[]`
+              action = 'lua print(3)',
+            },
+          },
+          footer = {}, --your footer
+        },
+      }
+    end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+  },
+}
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -158,7 +158,8 @@ return {
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
+        jdtls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --

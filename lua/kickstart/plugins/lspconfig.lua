ae088f5f0e04ce25e9aac57e584942bc180f898d
Fynn Friedrichs <fynn091209@proton.me>
2024-08-01 15:11:53 +0200

    lsp changes

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -167,6 +167,7 @@ return {
         pyright = {},
         jdtls = {},
         texlab = {},
+        bashls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --

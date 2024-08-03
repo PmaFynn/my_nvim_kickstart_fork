bfe98911b73deb140a79f56bd5cc0d39cdf4d701
Fynn Friedrichs <fynn091209@proton.me>
2024-08-03 12:28:57 +0200

    less lsp

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -168,11 +168,11 @@ return {
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        pyright = {},
-        jdtls = {},
+        -- pyright = {},
+        -- jdtls = {},
         texlab = {},
-        bashls = {},
-        -- rust_analyzer = {},
+        -- bashls = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:

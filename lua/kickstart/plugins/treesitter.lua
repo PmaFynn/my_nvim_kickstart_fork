4bafb5b83de0b4d6e5396735733be928925c36c0
fynn <fynn091209@proton.me>
2024-08-09 10:08:04 +0200

    disabled latex in treesitter so lsp can do the work

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -8,12 +8,13 @@ return {
       auto_install = true,
       highlight = {
         enable = true,
+        disable = { 'latex' }, -- Disable Tree-sitter for specific file types
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = true, disable = { 'ruby', 'markdown', 'text' } }, -- Also disable indent for these file types
     },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
@@ -21,8 +22,7 @@ return {
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup(opts)
 
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
+      -- Additional nvim-treesitter modules you can use to interact with nvim-treesitter:
       --
       --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
       --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context

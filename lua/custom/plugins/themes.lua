c525c2f95944a30aaa75b6c9193a06e18df8cfad
fynn <fynn091209@proton.me>
2024-08-10 14:10:49 +0200

    more themes

---
+++ lua/custom/plugins/themes.lua
@@ -0,0 +1,15 @@
+return {
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 43 },
+  { 'rebelot/kanagawa.nvim', name = 'kanagawa', priority = 899 },
+  { 'rose-pine/neovim', name = 'rose-pine' },
+  { 'baliestri/aura-theme', name = 'aura' },
+  -- {
+  --   "baliestri/aura-theme"
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function(plugin)
+  --     vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+  --     vim.cmd([[colorscheme aura-dark]])
+  --   end
+  -- },
+}

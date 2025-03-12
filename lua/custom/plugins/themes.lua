632a836362d417190f31fd254edeb42ef5f17c70
Fynn <fynn091209@proton.me>
2025-03-12 15:43:19 +0100

    rewrote theme logic

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -2,6 +2,18 @@ return {
   { 'catppuccin/nvim', priority = 43 },
   { 'rebelot/kanagawa.nvim', priority = 899 },
   { 'rose-pine/neovim', priority = 839 },
-  { 'nyoom-engineering/oxocarbon.nvim', priority = 894 },
+  {
+    'nyoom-engineering/oxocarbon.nvim',
+    --HACK: keep prio hightest for init function
+    priority = 2000,
+    --HACK: This is where the colorscheme is acutally set
+    init = function()
+      vim.o.background = 'light' -- or "light" for light mode
+      vim.cmd.colorscheme 'gruvbox'
+
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
   { 'navarasu/onedark.nvim', priority = 299 },
+  { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true, opts = ... },
 }
